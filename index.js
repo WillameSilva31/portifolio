018071d84566731234a1f1e43572a8354c02d219
willame <willamesilvaop@hotmail.com>
2025-03-13 18:35:15 -0300

    language button

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function copyToClickBoard(){
-    const content = document.getElementById('button_email').innerHTML;
-
-    navigator.clipboard.writeText(content)
-       .then(() => {
-        console.log("Text copied to clipboard...")
-        alert("e-mail copiado!")
-     })
-        .catch(err => {
-        console.log('Something went wrong', err);
-     })
- 
+document.addEventListener('DOMContentLoaded', function () {
+   changeLanguage('pt');
+});
+function changeLanguage(lang) { 
+   const elements = document.querySelectorAll('[data-pt], [data-en]');
+   elements.forEach(element => {
+       if (lang === 'pt' && element.dataset.pt) {
+           element.textContent = element.dataset.pt;
+       } else if (lang === 'en' && element.dataset.en) {
+           element.textContent = element.dataset.en; 
+       }
+   });
 }
+document.getElementById('btn-portuguese').addEventListener('click', () => {
+   changeLanguage('pt'); 
+});
+document.getElementById('btn-english').addEventListener('click', () => {
+   changeLanguage('en'); 
+});
